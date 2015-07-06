c481191ebc484fb311fac8c65d576276c3361331
squeek <squeek502@hotmail.com>
2015-07-05 18:49:34 -0700

    Add share links to homepage and infographics page  * Closes #3

--- config.rb
+++ config.rb
@@ -15,9 +15,31 @@ end
 
 page "data/*", :layout => :data_layout
 
+set :domain, 'http://considerveganism.com'
+
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def absolute_url(relative_url)
+    config[:domain] + "/" + (relative_url[0] == '/' ? relative_url[1..-1] : relative_url)
+  end
+
+  def share_image_to(external_url, internal_url, title, description)
+    share_links = {
+      :twitter => "https://twitter.com/intent/tweet?url=#{CGI.escape(external_url)}&text=#{CGI.escape(title)}",
+      :googleplus => "https://plus.google.com/share?url=#{CGI.escape(external_url)}",
+      :tumblr => "https://www.tumblr.com/widgets/share/tool?posttype=photo&content=#{CGI.escape(external_url)}&caption=#{CGI.escape(title)}&canonicalUrl=#{CGI.escape(absolute_url("/"))}",
+      :reddit => "https://www.reddit.com/submit?url=#{CGI.escape(external_url)}&title=#{CGI.escape(title)}"
+      # facebook doesn't have support for plain link image sharing via URL
+    }
+  end
+
+  def share_link_to(url, title, description)
+    share_links = {
+      :facebook => "https://www.facebook.com/sharer/sharer.php?u=#{CGI.escape(url)}",
+      :twitter => "https://twitter.com/intent/tweet?url=#{CGI.escape(url)}&text=#{CGI.escape(title)}",
+      :googleplus => "https://plus.google.com/share?url=#{CGI.escape(url)}",
+      :tumblr => "https://www.tumblr.com/widgets/share/tool?posttype=link&content=#{CGI.escape(url)}&title=#{CGI.escape(title)}&caption=#{CGI.escape(description)}",
+      :reddit => "https://www.reddit.com/submit?url=#{CGI.escape(url)}&title=#{CGI.escape(title)}"
+    }
+  end
+end
