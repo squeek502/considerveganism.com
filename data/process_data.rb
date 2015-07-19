69619dfc9fba12a6c4f212f7930226a95ae86140
squeek <squeek502@hotmail.com>
2015-07-18 19:13:43 -0700

    refactor: Move existing slaughter data to usa heading to make way for world data

--- data/process_data.rb
+++ data/process_data.rb
@@ -1,40 +1,40 @@
 # notes:
 #  when creating a new key, duplicate an existing hash so that we still get the dot notation that the yaml-parsed data has
 
-def process_slaughter_data(data)
+def process_usa_slaughter_data(data)
   # generate layer hen totals using monthly data
-  data.slaughter.chickens.layers.sold["total_2013"] = data.slaughter.chickens.layers.sold.by_month_2013.values.inject(:+)
-  data.slaughter.chickens.layers.sold["total_2014"] = data.slaughter.chickens.layers.sold.by_month_2014.values.inject(:+)
-  data.slaughter.chickens.layers.disappeared["total_2013"] = data.slaughter.chickens.layers.disappeared.by_month_2013.values.inject(:+)
-  data.slaughter.chickens.layers.disappeared["total_2014"] = data.slaughter.chickens.layers.disappeared.by_month_2014.values.inject(:+)
-  data.slaughter.chickens.layers.pullets_added["total_2013"] = data.slaughter.chickens.layers.pullets_added.by_month_2013.values.inject(:+)
-  data.slaughter.chickens.layers.pullets_added["total_2014"] = data.slaughter.chickens.layers.pullets_added.by_month_2014.values.inject(:+)
-  data.slaughter.chickens.layers["total_2013"] = data.slaughter.chickens.layers.sold.total_2013 + data.slaughter.chickens.layers.disappeared.total_2013
-  data.slaughter.chickens.layers["total_2014"] = data.slaughter.chickens.layers.sold.total_2014 + data.slaughter.chickens.layers.disappeared.total_2014
+  data.slaughter.usa.chickens.layers.sold["total_2013"] = data.slaughter.usa.chickens.layers.sold.by_month_2013.values.inject(:+)
+  data.slaughter.usa.chickens.layers.sold["total_2014"] = data.slaughter.usa.chickens.layers.sold.by_month_2014.values.inject(:+)
+  data.slaughter.usa.chickens.layers.disappeared["total_2013"] = data.slaughter.usa.chickens.layers.disappeared.by_month_2013.values.inject(:+)
+  data.slaughter.usa.chickens.layers.disappeared["total_2014"] = data.slaughter.usa.chickens.layers.disappeared.by_month_2014.values.inject(:+)
+  data.slaughter.usa.chickens.layers.pullets_added["total_2013"] = data.slaughter.usa.chickens.layers.pullets_added.by_month_2013.values.inject(:+)
+  data.slaughter.usa.chickens.layers.pullets_added["total_2014"] = data.slaughter.usa.chickens.layers.pullets_added.by_month_2014.values.inject(:+)
+  data.slaughter.usa.chickens.layers["total_2013"] = data.slaughter.usa.chickens.layers.sold.total_2013 + data.slaughter.usa.chickens.layers.disappeared.total_2013
+  data.slaughter.usa.chickens.layers["total_2014"] = data.slaughter.usa.chickens.layers.sold.total_2014 + data.slaughter.usa.chickens.layers.disappeared.total_2014
 
   # for every pullet added, a male chick was presumably hatched
-  data.slaughter.chickens.layers["male_chicks"] = data.slaughter.chickens.layers.dup.clear
-  data.slaughter.chickens.layers.male_chicks["total_2013"] = data.slaughter.chickens.layers.pullets_added.total_2013
-  data.slaughter.chickens.layers.male_chicks["total_2014"] = data.slaughter.chickens.layers.pullets_added.total_2014
+  data.slaughter.usa.chickens.layers["male_chicks"] = data.slaughter.usa.chickens.layers.dup.clear
+  data.slaughter.usa.chickens.layers.male_chicks["total_2013"] = data.slaughter.usa.chickens.layers.pullets_added.total_2013
+  data.slaughter.usa.chickens.layers.male_chicks["total_2014"] = data.slaughter.usa.chickens.layers.pullets_added.total_2014
 
   # get total dairy cows slaughtered, as only the inspected slaughter data is broken into classifications
-  data.slaughter.cattle.dairy_cows["percent_of_total_2014"] = data.slaughter.cattle.dairy_cows.total_inspected_2014 / (data.slaughter.cattle.total_inspected_2014 - data.slaughter.cattle.veal_calves.total_inspected_2014).to_f
-  data.slaughter.cattle.dairy_cows["percent_of_total_2013"] = data.slaughter.cattle.dairy_cows.total_inspected_2013 / (data.slaughter.cattle.total_inspected_2013 - data.slaughter.cattle.veal_calves.total_inspected_2013).to_f
-  data.slaughter.cattle.dairy_cows["total_2014"] = (data.slaughter.cattle.dairy_cows.percent_of_total_2014 * data.slaughter.cattle.total_2014).to_i
-  data.slaughter.cattle.dairy_cows["total_2013"] = (data.slaughter.cattle.dairy_cows.percent_of_total_2013 * data.slaughter.cattle.total_2013).to_i
+  data.slaughter.usa.cattle.dairy_cows["percent_of_total_2014"] = data.slaughter.usa.cattle.dairy_cows.total_inspected_2014 / (data.slaughter.usa.cattle.total_inspected_2014 - data.slaughter.usa.cattle.veal_calves.total_inspected_2014).to_f
+  data.slaughter.usa.cattle.dairy_cows["percent_of_total_2013"] = data.slaughter.usa.cattle.dairy_cows.total_inspected_2013 / (data.slaughter.usa.cattle.total_inspected_2013 - data.slaughter.usa.cattle.veal_calves.total_inspected_2013).to_f
+  data.slaughter.usa.cattle.dairy_cows["total_2014"] = (data.slaughter.usa.cattle.dairy_cows.percent_of_total_2014 * data.slaughter.usa.cattle.total_2014).to_i
+  data.slaughter.usa.cattle.dairy_cows["total_2013"] = (data.slaughter.usa.cattle.dairy_cows.percent_of_total_2013 * data.slaughter.usa.cattle.total_2013).to_i
 
   # calculate beef cattle slaughter by excluding dairy and veal
-  data.slaughter.cattle["beef_cattle"] = data.slaughter.cattle.dairy_cows.dup.clear
-  data.slaughter.cattle.beef_cattle["total_2014"] = data.slaughter.cattle.total_2014 - data.slaughter.cattle.dairy_cows.total_2014
-  data.slaughter.cattle.beef_cattle["total_2013"] = data.slaughter.cattle.total_2013 - data.slaughter.cattle.dairy_cows.total_2013
+  data.slaughter.usa.cattle["beef_cattle"] = data.slaughter.usa.cattle.dairy_cows.dup.clear
+  data.slaughter.usa.cattle.beef_cattle["total_2014"] = data.slaughter.usa.cattle.total_2014 - data.slaughter.usa.cattle.dairy_cows.total_2014
+  data.slaughter.usa.cattle.beef_cattle["total_2013"] = data.slaughter.usa.cattle.total_2013 - data.slaughter.usa.cattle.dairy_cows.total_2013
 
   # get total lambs slaughtered, as only the inspected slaughter data is broken into classifications
-  data.slaughter.sheep.lambs["percent_of_total_2014"] = data.slaughter.sheep.lambs.total_inspected_2014 / data.slaughter.sheep.total_inspected_2014.to_f
-  data.slaughter.sheep.lambs["percent_of_total_2013"] = data.slaughter.sheep.lambs.total_inspected_2013 / data.slaughter.sheep.total_inspected_2013.to_f
-  data.slaughter.sheep.lambs["total_2014"] = (data.slaughter.sheep.lambs.percent_of_total_2014 * data.slaughter.sheep.total_2014).to_i
-  data.slaughter.sheep.lambs["total_2013"] = (data.slaughter.sheep.lambs.percent_of_total_2013 * data.slaughter.sheep.total_2013).to_i
+  data.slaughter.usa.sheep.lambs["percent_of_total_2014"] = data.slaughter.usa.sheep.lambs.total_inspected_2014 / data.slaughter.usa.sheep.total_inspected_2014.to_f
+  data.slaughter.usa.sheep.lambs["percent_of_total_2013"] = data.slaughter.usa.sheep.lambs.total_inspected_2013 / data.slaughter.usa.sheep.total_inspected_2013.to_f
+  data.slaughter.usa.sheep.lambs["total_2014"] = (data.slaughter.usa.sheep.lambs.percent_of_total_2014 * data.slaughter.usa.sheep.total_2014).to_i
+  data.slaughter.usa.sheep.lambs["total_2013"] = (data.slaughter.usa.sheep.lambs.percent_of_total_2013 * data.slaughter.usa.sheep.total_2013).to_i
 end
 
-def process_inventory_data(data)
-  data.inventory.cattle["dairy_calves_born"] = data.inventory.cattle.dairy_cows.dup
+def process_usa_inventory_data(data)
+  data.inventory.usa.cattle["dairy_calves_born"] = data.inventory.usa.cattle.dairy_cows.dup
 end
