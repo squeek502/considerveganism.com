2b7b6ece33a59bbb23b17b28fa89cab8ed4cbdc8
Ryan Liptak <squeek502@hotmail.com>
2015-08-28 17:45:24 -0700

    Start on integrating middleman-blog

--- Gemfile
+++ Gemfile
@@ -7,6 +7,12 @@ gem "middleman", "~>3.3.11"
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
 
+# blog plugin
+gem "middleman-blog", "~> 3.5.3"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
+
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
--- config.rb
+++ config.rb
@@ -1,3 +1,7 @@
+###
+# General settings
+###
+
 activate :directory_indexes
 
 set :css_dir, 'stylesheets'
@@ -16,6 +20,43 @@ end
 page "data/*", :layout => :data_layout
 
 set :domain, 'http://considerveganism.com'
+
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "blog_layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
+# Helpers
+###
 
 # Methods defined in the helpers block are available in templates
 helpers do
